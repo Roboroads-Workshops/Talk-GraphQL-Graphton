2dc183a7bc74b6b87bcd4bd7b5f0e1dd0be1a692
Robbin Schepers <robbin@deschepers.nl>
2022-08-18 23:15:32 +0200

    feat: Prepare home page for graphton output

--- graphton-app/src/routes/+page.svelte
+++ graphton-app/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {onMount} from "svelte";
+    import axios from "axios";
+
+    let graphtonOutput = '// Nothing yet!';
+
+    onMount(() => {
+        axios.post('http://localhost/graphql', {query: 'query { user(id: 69) { name posts { title } } }'})
+            .then(response => {
+                graphtonOutput = JSON.stringify(response.data, null, 2);
+            });
+    });
+</script>
+
+<h1>Graphton output</h1>
+
+<pre><code>{graphtonOutput}</code></pre>
